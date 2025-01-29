57272401500b26d52da24d8b3a2281d5d87560f0
enikar <enikar@gresille.org>
2025-01-29 01:34:01 +0100

    Add a solution for day 2.

---
+++ hday2.hs
@@ -0,0 +1,60 @@
+-- AoC 2021, day 2
+
+{-# LANGUAGE LambdaCase #-}
+{- HLINT ignore "Eta reduce" -}
+
+import System.IO (readFile')
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
+import Data.List (foldl')
+
+data Instruction = Forward Int
+                 | Down Int
+                 | Up Int
+
+type Instructions = [Instruction]
+
+getDatas :: String -> IO Instructions
+getDatas filename = parseInstructions <$> readFile' filename
+
+parseInstructions :: String -> Instructions
+parseInstructions str = map parseInstruction (lines str)
+  where
+    parseInstruction s = case words s of
+      ["forward", n] -> Forward (readInt n)
+      ["down", n]   -> Down (readInt n)
+      ["up", n]     -> Up (readInt n)
+      _             -> error "Error: parseInstruction: invalid input"
+
+    readInt s = fromMaybe errorReadInt (readMaybe s)
+      where
+        errorReadInt = error ("Error: readInt: not an Int: " <> s)
+
+printSolution :: Show a => String -> a -> IO ()
+printSolution part sol = putStrLn (part <> ": " <> show sol)
+
+main :: IO ()
+main = do
+  instrs <- getDatas "day2.txt"
+  printSolution "Part1" (part1 instrs)
+  printSolution "Part2" (part2 instrs)
+
+part1 :: Instructions -> Int
+part1 instrs = getResult (foldl' f (0, 0) instrs)
+  where
+    getResult (x,z) = x*z
+
+    f (x, z) = \case
+      Forward n -> (x+n, z)
+      Down    n -> (x, z+n)
+      Up      n -> (x, z-n)
+
+part2 :: Instructions -> Int
+part2 instrs = getResult (foldl' f (0, 0, 0) instrs)
+  where
+    getResult (x, z, _) = x*z
+
+    f (x, z, a) = \case
+        Forward n -> (x+n, z+n*a, a)
+        Down    n -> (x, z, a+n)
+        Up      n -> (x, z, a-n)
