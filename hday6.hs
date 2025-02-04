c80ead198c1c0e714b58cc90f857249535048aee
enikar <enikar@gresille.org>
2025-02-04 22:08:10 +0100

    Add an alternative way to parse input.

--- hday6.hs
+++ hday6.hs
@@ -8,6 +8,7 @@ import Data.Word (Word8)
 
 import Data.List (foldl')
 import Data.Functor (($>))
+import Control.Monad.Loops (unfoldM)
 import Data.IntMap.Strict (IntMap)
 import Data.IntMap.Strict qualified as M
 
@@ -21,8 +22,8 @@ import Scanner
   ,decimal
   )
 
+-- we use an IntMap to count occurrences of different timers.
 type LanternFishes = IntMap Int
-
 
 printSolution :: Show a => String -> a -> IO ()
 printSolution part sol = putStrLn (part <> ": " <> show sol)
@@ -33,25 +34,27 @@ main = do
   printSolution "Part1" (partx 80 fishes)
   printSolution "Part2" (partx 256 fishes)
 
+-- Since InMaps are Foldable, we can use sum to add
+-- all values to count lanternfishes.
 partx :: Int -> IntMap Int -> Int
-partx n fishes = M.foldl' (+) 0 (times n next fishes)
+partx n fishes = sum (times n next fishes)
 
 next :: LanternFishes -> LanternFishes
 next fishes = M.foldlWithKey' f M.empty fishes
   where
-    update n Nothing = Just n
+    update n Nothing  = Just n
     update n (Just x) = Just (n+x)
 
     f acc timer n
       |timer == 0 = M.insert 8 n (M.alter (update n) 6 acc)
       |timer == 7 = M.alter (update n) 6 acc
-      |otherwise = M.insert (timer - 1) n acc
+      |otherwise  = M.insert (timer - 1) n acc
 
 -- borrow from: https://github.com/glguy/advent/blob/main/common/src/Advent/Prelude.hs
 times :: Int -> (a -> a) -> a -> a
 times n f x
-  | n <= 0    = x
-  | otherwise = times (n-1) f $! f x
+  |n <= 0    = x
+  |otherwise = times (n-1) f $! f x
 
 -- Parsing stuff
 getDatas :: String -> IO LanternFishes
@@ -61,7 +64,7 @@ parseDatas :: ByteString -> LanternFishes
 parseDatas str =
   either error
          id
-         (scanOnly parseMap str)
+         (scanOnly lanternFishes str)
 
 charToWord8 :: Char -> Word8
 charToWord8 = fromIntegral . fromEnum
@@ -70,20 +73,60 @@ eol,comma :: Word8
 eol = charToWord8 '\n'
 comma = charToWord8 ','
 
-parseMap :: Scanner LanternFishes
-parseMap = buildMap <$> parseLine
+-- The parsing is simple. There is just one line of digits
+-- separated by comma.
+lanternFishes :: Scanner LanternFishes
+lanternFishes = buildMap <$> yaParseLine
 
+-- recursive parser to collect the digits.
 parseLine :: Scanner [Int]
 parseLine = do
   c <- lookAhead
   case c of
-    Nothing             -> pure []
-    Just v | v == comma -> word8 v *> parseLine
-           | v == eol   -> word8 v $> []
-           | otherwise  -> do
-               d <- decimal
-               ns <- parseLine
-               pure (d:ns)
+    Nothing            -> pure []
+    Just v |v == comma -> word8 v *> parseLine
+           |v == eol   -> word8 v $> []
+           |otherwise  -> do
+              d <- decimal
+              ns <- parseLine
+              pure (d:ns)
+
+-- Alternative version using Control.Monad.Loops.unFoldM
+-- With this one we parse digits separated by any characters.
+-- while parseLine par decimal number separated by ',' or
+-- '\n'.
+-- In the two cases, we don't exactly parse the input string even
+-- if they work.
+yaParseLine :: Scanner [Int]
+yaParseLine = unfoldM parsedDigit
+  where
+    -- Note: here (digit <* optionalChar) doesn't work.
+    -- We need a monad, due to the use of unfoldM.
+    parsedDigit = do
+      n <- digit
+      optionalChar
+      pure n
+
+digits :: [Word8]
+digits = map charToWord8 ['0'..'9']
+
+isDigit :: Word8 -> Bool
+isDigit d = d `elem` digits
+
+digit :: Scanner (Maybe Int)
+digit = do
+  c <- lookAhead
+  case c of
+    Just d |isDigit d -> word8 d
+                         $> Just (fromIntegral d - fromEnum '0')
+    _                 -> pure Nothing
+
+optionalChar :: Scanner ()
+optionalChar = do
+  c <- lookAhead
+  case c of
+    Nothing -> pure ()
+    Just v  -> word8 v $> ()
 
 buildMap :: [Int] -> LanternFishes
 buildMap ns = foldl' f M.empty ns
