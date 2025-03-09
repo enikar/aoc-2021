3fff42b8845fcf416add246c106df6ca59710fad
enikar <enikar@gresille.org>
2025-03-09 13:40:32 +0100

    Minor improvement.

--- hday7.hs
+++ hday7.hs
@@ -5,12 +5,15 @@
 import System.IO (readFile')
 import Data.Maybe (fromMaybe)
 import Text.Read (readMaybe)
-import Data.List.Split (splitOn)
+import Data.List.Extra
+  (splitOn
+  ,trimEnd -- to removed an eventually end of line.
+  )
 
 getDatas :: String -> IO [Int]
 getDatas filename = parse <$> readFile' filename
   where
-    parse str = map readInt (splitOn "," str)
+    parse str = map readInt (splitOn "," (trimEnd str))
     readInt s = fromMaybe errReadInt (readMaybe s)
       where
         errReadInt = error ("Error: readInt: not an Int: " <> s)
@@ -27,7 +30,9 @@ main = do
 part1 :: [Int] -> Int
 part1 nums = minimum (map sumCosts nums)
   where
-    sumCosts n = sum (map (\x -> abs (n - x)) nums)
+    sumCosts n = sum (map (cost n) nums)
+
+    cost n x = abs (n - x)
 
 part2 :: [Int] -> Int
 part2 nums = minimum (map sumCosts nums)
